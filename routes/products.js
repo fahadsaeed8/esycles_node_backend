466b2c751358ac4d1f5c04d18eaacc29a100d5aa
Tabarak <tabaraksohail96@gmail.com>
2026-01-18 22:23:47 +0500

    feat: send-proper-error

--- routes/products.js
+++ routes/products.js
@@ -181,8 +181,9 @@ router.get(
         .populate("model brand color seller");
 
       if (!products.length) {
-        return res.status(404).json({
+        return res.status(200).json({
           success: false,
+          data: [],
           message: `No products found for type "${type}"`,
         });
       }
